150b72f4824b4f05402b5ef0eecf15005f615eaa
Leyva <luisleyva.bq@gmail.com>
2023-12-08 18:10:14 -0600

    finish preprocessing data

--- train_chatbot.py
+++ train_chatbot.py
@@ -7,7 +7,7 @@ from keras.optimizers import SGD # Stochastic Gradient Descent
 import random
 
 import nltk # Natural Language Toolkit
-from nltk.stem import WordNetLemmatizer # Lemmatizer
+from nltk.stem import WordNetLemmatizer # lemmatizer
 lemmatizer = WordNetLemmatizer()
 import json
 import pickle
@@ -34,4 +34,20 @@ for intent in intents['intents']:
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-print(documents)
+# lemmatizing each word
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_letters]
+words = sorted(list(set(words)))
+
+# sort classes
+classes = sorted(list(set(classes)))
+
+# documents = combination between patterns and intents
+print (len(documents), "documents")
+
+# classes = intents
+print (len(classes), "classes", classes)
+
+# words = all words, vocabulary
+print (len(words), "unique lemmatized words", words)
+
+pickle.dump(words, open('words.pkl', 'wb')) # wb = write binary
